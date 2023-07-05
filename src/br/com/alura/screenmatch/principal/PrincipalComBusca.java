fdc9dc18ea2fbadb291e19935d22e47751c25883
Ricardo Rolim de Oliveira <ricardorolim27@gmail.com>
2023-07-05 10:09:35 -0300

    Create PrincipalComBusca.java
    Cria classe PrincipalComBusca para fazer request para uma api de filmes.
    
    Usado HttpRequest e HttpResponse

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,33 @@
+package br.com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		System.out.println("Digite um filme para a busca:");
+		
+		Scanner leitura = new Scanner(System.in);
+		
+		String busca = leitura.next();
+		
+		String endereco = "https://www.omdbapi.com/?apikey=eadba239&t=";
+
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create(endereco + busca)).build();
+
+		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+		
+		System.out.println(response.body());
+
+	}
+
+}
